aa66898cc29996ad468188941852c8bb5ffcedfa
Евгений Шкумат <kotbmasle@gmail.com>
2024-02-03 21:26:24 +0300

    add and use find_product_in_num_column. Final version of the project.

--- Basic36/Logic.cpp
+++ Basic36/Logic.cpp
@@ -26,12 +26,12 @@ string find_even_column(int matrix[N][M]) {
 	return msg;
 }
 
-int find_column_sum(int matrix[N][M], int column_index) {
+int find_column_abs_sum(int matrix[N][M], int column_index) {
 	int sum = 0;
 
 	for (int i = 0; i < N; i++)
 	{
-		sum += matrix[i][column_index];
+		sum += abs(matrix[i][column_index]);
 	}
 
 	return sum;
@@ -45,7 +45,7 @@ string find_digaonal_column_sum(int matrix[N][M]) {
 		int sum = 0;
 
 		if (matrix[i][i] < 0) {
-			sum += find_column_sum(matrix, i);
+			sum += find_column_abs_sum(matrix, i);
 			msg += "Column: " + to_string(i + 1) + " Sum: " + to_string(sum) + "\n";
 		}
 	}
@@ -57,15 +57,45 @@ string find_digaonal_column_sum(int matrix[N][M]) {
 	return msg;
 }
 
-int find_column_abs_sum(int matrix[N][M], int column_index) {
-	int sum = 0;
+int find_column_product(int matrix[N][M], int column_index) {
+	int product = 1;
 
 	for (int i = 0; i < N; i++)
 	{
-		sum += abs(matrix[i][column_index]);
+		product *= matrix[i][column_index];
 	}
 
-	return sum;
+	return product;
 }
 
+int find_product_in_num_column(int matrix[N][M], int num) {
+	int min_product = 2147483646;
+	int column_index = -2;
+
+	for (int i = 0; i < M; i++)
+	{
+		bool flag = true;
+		int current_product = 1;
+
+		for (int j = 0; j < N; j++)
+		{
+			if (abs(matrix[j][i]) > num) {
+				current_product = 1;
+				break;
+			}
+			current_product *= matrix[j][i];
+		}
+
+		if (current_product == 1) {
+			current_product = 2147483646;
+		}
+
+		if (current_product < min_product) {
+			min_product = current_product;
+			column_index = i;
+		}
+	}
+
+	return column_index;
+}
 
--- Basic36/Main.cpp
+++ Basic36/Main.cpp
@@ -9,16 +9,18 @@ string convert_matrix_to_string(int matrix[N][M]);
 string find_even_column(int matrix[N][M]);
 string find_digaonal_column_sum(int matrix[N][M]);
 int max_abs_sum_row(int matrix[N][M]);
+int find_product_in_num_column(int matrix[N][M], int num);
 
 int main() {
 	int matrix[N][M];
 
 	random_matrix_init(matrix, 1, 9);
 
+	int num = 7;
 
 	cout << "Matrix:\n" << convert_matrix_to_string(matrix);
-	cout << "Rows that have max abs sum and they have only odd elements(-1 = none): "
-		<< max_abs_sum_row(matrix) + 1;
+	cout << "Columns that have min product and they have only elements that are not more than "
+		<< num << "(-1 = none): " << find_product_in_num_column(matrix, num) + 1;
 
 	return 0;
 }
